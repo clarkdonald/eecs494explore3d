defcbebabc808131cd35823391e58536568596f3
Donald Clark <clarkds@umich.edu>
2013-11-01 07:01:23 -0400

    got rid of score comments

--- jni/application/Crosshair.cpp
+++ jni/application/Crosshair.cpp
@@ -32,39 +32,29 @@ void Crosshair::render(const bool &is_wielding_weapon, Player* player) {
   int k;
   for(k = 0; k < player->get_health(); k++){
 	render_image("filled_heart", Point2f(x_val, 0.0f), Point2f(x_val + 32.0f, 32.0f));
-	x_val += 32.0f;
+	  x_val += 32.0f;
   }
   while(k < 5){
 	render_image("empty_heart", Point2f(x_val, 0.0f), Point2f(x_val + 32.0f, 32.0f));
-	x_val += 32;
+	  x_val += 32;
     k++;
   }
 
   x_val = 0;
   if(player -> can_lift()){
     render_image("yellow_pill", Point2f(x_val, 32.0f), Point2f(x_val + 32.0f, 64.0f));
-	x_val += 32;
+	  x_val += 32;
   }
   if(player -> can_walk_through_fire()){
     render_image("blue_pill", Point2f(x_val, 32.0f), Point2f(x_val + 32.0f, 64.0f));
-	x_val += 32;
+	  x_val += 32;
   }
   if(player -> can_walk_through_terrain()){
     render_image("white_pill", Point2f(x_val, 32.0f), Point2f(x_val + 32.0f, 64.0f));
-	x_val += 32;
+	  x_val += 32;
   }
   if(player -> can_jump()){
     render_image("green_pill", Point2f(x_val, 32.0f), Point2f(x_val + 32.0f, 64.0f));
-	x_val += 32;
+	  x_val += 32;
   }
-
-
-  /*std::string score_ = "Score: ";
-  score_ += std::to_string(monsters_killed);
-  String score = "";
-  score = score_;
-  Zeni::Font &fr = get_Fonts()["system_36_x600"];
-  fr.render_text(score, Point2f(0.0f, 0.0f), get_Colors()["title_text"], ZENI_LEFT);
-  */
-
 }
